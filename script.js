d6fea2ada1562c6f4817958bbff58c06cf6ac360
Bohdan Nosachenko <bohdannosachenko@Bohdans-MBP.x1group>
2019-12-12 17:10:47 +0200

    white snow for edge

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
   var frontCard = document.getElementById('cardFront');
+  var backCard = document.getElementById('cardBack');
   var wrapper = document.getElementById('cardWrapper');
   var card = document.getElementsByClassName('card')[0];
   var music = document.getElementById("myAudio");
   var stopped = false;
 
+  function isFuckingIE() {
+    const ua = window.navigator.userAgent;
+    const msie = ua.indexOf('MSIE ');
+    return (msie > 0 || window.navigator.userAgent.indexOf('Trident/') > 0);
+  }
+
   frontCard.addEventListener("click", function (event) {
-     wrapper.classList.add("active");
+    wrapper.classList.add("active");
     card.classList.add("flipped");
+    if (isFuckingIE()) {
+      setTimeout(function () {
+        backCard.style['backface-visibility'] = 'visible';
+      }, 450);
+    }
+
     if (!stopped) {
       music.play();
     }
   });
 
-  var backCard = document.getElementsByClassName('back')[0];
-
   backCard.addEventListener("click", function (event) {
     event.stopPropagation();
     wrapper.classList.remove("active");
     card.classList.remove("flipped");
+    if (isFuckingIE()) {
+      setTimeout(function () {
+        backCard.style['backface-visibility'] = 'hidden';
+      }, 450);
+    }
   });
 
   var soundImgWrapper = document.getElementsByClassName('img-wrapper')[0];
