c99e42e50162f753369a9649eb459b224af5cff3
Bohdan Nosachenko <bohdannosachenko@Bohdans-MBP.x1group>
2019-12-12 09:58:04 +0200

    work in progress

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ document.addEventListener('DOMContentLoaded', function () {
   var frontCard = document.getElementById('cardFront');
   var wrapper = document.getElementById('cardWrapper');
   var card = document.getElementsByClassName('card')[0];
+  var music = document.getElementById("myAudio");
 
   frontCard.addEventListener("click", function () {
     wrapper.classList.add("active");
     card.classList.add("flipped");
+    music.play();
   });
 
   var backCard = document.getElementsByClassName('back')[0];
@@ -14,5 +16,13 @@ document.addEventListener('DOMContentLoaded', function () {
     event.stopPropagation();
     wrapper.classList.remove("active");
     card.classList.remove("flipped");
+  });
+
+  var soundButton = document.getElementById('soundButton');
+  
+  console.log('soundButton', soundButton);
+  soundButton.addEventListener("click", function (event) {
+    console.log('sound button');
+    music.muted = !music.muted;
   });
 });
